0c7dfbf1c5f1ea8d4b59562605d9859b2aa42317
luix <luiz_santos1811@outlook.com>
2026-01-30 10:00:41 -0300

    feat: add StartingClass enum

--- app/src/main/java/com/luix/eldenbuilds/data/model/StartingClass.java
+++ app/src/main/java/com/luix/eldenbuilds/data/model/StartingClass.java
@@ -1,4 +1,24 @@
 package com.luix.eldenbuilds.data.model;
 
+import lombok.Getter;
+
+@Getter
 public enum StartingClass {
+    VAGABOND("Vagabond"),
+    WARRIOR("Warrior"),
+    HERO("Hero"),
+    BANDIT("Bandit"),
+    ASTROLOGER("Astrologer"),
+    PROPHET("Prophet"),
+    SAMURAI("Samurai"),
+    PRISONER("Prisoner"),
+    CONFESSOR("Confessor"),
+    WRETCH("Wretch");
+
+    private final String displayName;
+
+    StartingClass(String displayName) {
+        this.displayName = displayName;
+    }
+
 }
