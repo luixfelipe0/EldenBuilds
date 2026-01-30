5058fd0e6629dc87ecd422ce6afbb3ab2b56fafc
luix <luiz_santos1811@outlook.com>
2026-01-30 09:59:04 -0300

    feat: add room converters for StartingClass enum

--- app/src/main/java/com/luix/eldenbuilds/data/local/converters/Converters.java
+++ app/src/main/java/com/luix/eldenbuilds/data/local/converters/Converters.java
@@ -1,4 +1,24 @@
 package com.luix.eldenbuilds.data.local.converters;
 
+import androidx.room.TypeConverter;
+import com.luix.eldenbuilds.data.model.StartingClass;
+
 public class Converters {
+
+    @TypeConverter
+    public static StartingClass toStartingClass(String value) {
+        if (value == null) {
+            return StartingClass.WRETCH;
+        }
+        try {
+            return StartingClass.valueOf(value);
+        } catch (IllegalArgumentException e) {
+            return StartingClass.WRETCH;
+        }
+    }
+
+    @TypeConverter
+    public static String fromStartingClass(StartingClass startingClass) {
+        return startingClass == null ? null : startingClass.name();
+    }
 }
