66ac6bc64f9308ac2ea7eaa80b07c3964fa6ff96
luix <luiz_santos1811@outlook.com>
2026-03-30 17:29:22 -0300

    feat(builds): add BuildViewModelFactory for ViewModel constructor injection

--- app/src/main/java/com/luix/eldenbuilds/feature/builds/presentation/factory/BuildViewModelFactory.java
+++ app/src/main/java/com/luix/eldenbuilds/feature/builds/presentation/factory/BuildViewModelFactory.java
@@ -1,4 +1,30 @@
 package com.luix.eldenbuilds.feature.builds.presentation.factory;
 
-public class BuildViewModelFactory {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.luix.eldenbuilds.data.repository.BuildRepository;
+import com.luix.eldenbuilds.domain.usecase.GetUserBuildsUseCase;
+import com.luix.eldenbuilds.ui.viewmodel.BuildViewModel;
+
+public class BuildViewModelFactory implements ViewModelProvider.Factory {
+
+    private final BuildRepository buildRepository;
+    private final GetUserBuildsUseCase getUserBuildsUseCase;
+
+    public BuildViewModelFactory(GetUserBuildsUseCase getUserBuildsUseCase, BuildRepository buildRepository) {
+        this.getUserBuildsUseCase = getUserBuildsUseCase;
+        this.buildRepository = buildRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+
+        return (T) new BuildViewModel(getUserBuildsUseCase, buildRepository);
+
+    }
+
+
 }
